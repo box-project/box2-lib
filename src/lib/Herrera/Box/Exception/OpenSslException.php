45536a47c005e877a9388745b28b3e577ab7baf8
Kevin Herrera <kevin@herrera.io>
2013-03-07 08:46:07 -0800

    Adding ability to clear buffered OpenSSL error messages.

--- src/lib/Herrera/Box/Exception/OpenSslException.php
+++ src/lib/Herrera/Box/Exception/OpenSslException.php
@@ -18,4 +18,28 @@ class OpenSslException extends Exception
     {
         return new static(openssl_error_string());
     }
+
+    /**
+     * Clears the error buffer, preventing unassociated error messages from
+     * being used by the lastError() method. This is required for lsatError()
+     * to function properly. If the clearing loop continues beyond a certain
+     * number, a warning will be triggered before the loop is broken.
+     *
+     * @param integer $count The maximum number of rounds.
+     */
+    public static function reset($count = 100)
+    {
+        $counter = 0;
+
+        while (openssl_error_string()) {
+            if ($count < ++$counter) {
+                trigger_error(
+                    "The OpenSSL error clearing loop has exceeded $count rounds.",
+                    E_USER_WARNING
+                );
+
+                break;
+            }
+        }
+    }
 }
--- src/tests/Herrera/Box/Tests/Exception/OpenSslExceptionTest.php
+++ src/tests/Herrera/Box/Tests/Exception/OpenSslExceptionTest.php
@@ -15,10 +15,44 @@ class OpenSslExceptionTest extends TestCase
             );
         }
 
+        OpenSslException::reset();
+
         openssl_pkey_get_private('test', 'test');
 
         $exception = OpenSslException::lastError();
 
         $this->assertRegExp('/PEM routines/', $exception->getMessage());
+    }
+
+    public function testReset()
+    {
+        openssl_pkey_get_private('test', 'test');
+
+        OpenSslException::reset();
+
+        $this->assertEmpty(openssl_error_string());
+    }
+
+    public function testResetWarning()
+    {
+        openssl_pkey_get_private('test' . rand(), 'test' . rand());
+
+        restore_error_handler();
+
+        @OpenSslException::reset(0);
+
+        $error = error_get_last();
+
+        $this->assertEquals(
+            'The OpenSSL error clearing loop has exceeded 0 rounds.',
+            $error['message']
+        );
+    }
+
+    protected function setUp()
+    {
+        if (false === extension_loaded('openssl')) {
+            $this->markTestSkipped('The "openssl" extension is not available.');
+        }
     }
 }
