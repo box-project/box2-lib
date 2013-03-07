f12493799d21380af6cfe67220688d5dd8cd18aa
Kevin Herrera <kevin@herrera.io>
2013-03-06 16:35:06 -0800

    Adding Composer source code compactor.

---
+++ src/tests/Herrera/Box/Tests/Compactor/ComposerTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Herrera\Box\Compactor;
+
+use Herrera\Box\Compactor\Composer;
+use Herrera\PHPUnit\TestCase;
+
+class ComposerTest extends TestCase
+{
+    /**
+     * @var Composer
+     */
+    private $composer;
+
+    public function testCompact()
+    {
+        $original = <<<ORIGINAL
+<?php
+
+/**
+ * A comment.
+ */
+class AClass
+{
+    /**
+     * A comment.
+     */
+    public function aMethod()
+    {
+        \$test = true; # a comment
+    }
+}
+ORIGINAL;
+
+        $expected = <<<EXPECTED
+<?php
+
+
+
+
+class AClass
+{
+
+
+
+public function aMethod()
+{
+\$test = true;
+ }
+}
+EXPECTED;
+
+        $this->assertEquals($expected, $this->composer->compact($original));
+    }
+
+    public function testSupports()
+    {
+        $this->assertTrue($this->composer->supports('test.php'));
+    }
+
+    protected function setUp()
+    {
+        $this->composer = new Composer();
+    }
+}
